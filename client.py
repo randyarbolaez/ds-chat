eba32df3eda03f3ae88d287d08d60e5080938350
randyarbolaez <randyarbolaez@outlook.com>
2026-09-11 09:12:47 -0400

    multiple clients can connect | every client receives the messages

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import socket
-import asyncio
+import threading
 
 s = socket.socket()
 
@@ -10,5 +10,17 @@ s.connect(('127.0.0.1', port))
 name = input("What is your name? ")
 s.send(f"{name} has joined the chat".encode())
 
+def get_messages(s):
+    while True:
+        data = s.recv(1024)
+
+        if not data:
+            break
+        print({data.decode()})
+
+thread1 = threading.Thread(target=get_messages, args=(s,), daemon= True)
+thread1.start()
+
 while True:
     s.send(input(f"{name} >>> ").encode())
+    # thread1.join()
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import socket
 import threading
 
+addresses = {}
+clients = {}
+
 try:
     s = socket.socket()
     print("Socket is successfully created!")
@@ -11,16 +14,30 @@ port = 8080
 s.bind(('', port))
 s.listen(5)
 
-def messages(c,address):
+def get_message(c,address):
     print(f"Connection from {address}")
     while True:
         data = c.recv(1024)
 
         if not data:
             break
-        print(data.decode())
+        # blah = {f"{addresses[address]}":data.decode()}
+        # print({blah})
+        send_message(c, address, data)
+
+
+
+def send_message(c, from_address, msg):
+    for client in clients:
+        if client != from_address:
+            clients[client].sendall(msg)
+
 
 while True:
     c, address = s.accept()
-    thread = threading.Thread(target=messages, args=(c,address), daemon= True)
-    thread.start()
+    addresses[address] = c.recv(1024).decode().split(' ')[0]
+    clients[address] = c
+
+    print(addresses)
+    thread1 = threading.Thread(target=get_message, args=(c,address), daemon= True)
+    thread1.start()
